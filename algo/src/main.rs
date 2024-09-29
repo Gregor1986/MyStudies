5282332af0cf50f97cac2946f3778185ce64d9bb
Gregor1986 <egor953@yandex.ru>
2024-09-29 19:44:18 +0300

    Creation vector

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut a = vec![];
+    let len_vec = 100;
+    let mut i = 1;
+    while i <= len_vec {
+        a.push(i);
+        i += 1;
+    }
+    println!("{:?}", a);
 }
