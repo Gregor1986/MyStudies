13c0b69b4c5c073d6c382155b9dab7c7a6f30b19
Gregor1986 <egor953@yandex.ru>
2024-09-29 19:49:04 +0300

    Added function create vector

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
+    println!("{:?}", create_vec(1000));
+}
+fn create_vec(len_vec: i32) -> Vec<i32> {
     let mut a = vec![];
-    let len_vec = 100;
     let mut i = 1;
     while i <= len_vec {
         a.push(i);
         i += 1;
     }
-    println!("{:?}", a);
+    a
 }
