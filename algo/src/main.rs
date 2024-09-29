5c3baa64806fdb919599395153d2a39563b07f67
Gregor1986 <egor953@yandex.ru>
2024-09-29 20:37:04 +0300

    Added position element

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,5 +1,21 @@
 fn main() {
-    println!("{:?}", create_vec(1000));
+    let vec_a = create_vec(10000);
+    // println!("{:?}", vec_a);
+    let item = 5;
+    let mut low = 0;
+    let mut high = vec_a.len() - 1;
+    while low <= high {
+        let mid = (low + high) / 2;
+        let guess = vec_a[mid];
+        if guess == item {
+            println!("Позиция элемента в векторе {}", mid);
+            break;
+        } else if guess > item {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
 }
 fn create_vec(len_vec: i32) -> Vec<i32> {
     let mut a = vec![];
