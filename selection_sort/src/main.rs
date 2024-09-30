5d53f5626e6fe90527563a86ae7f5709094fe39f
Gregor1986 <egor953@yandex.ru>
2024-09-30 15:00:59 +0300

    Test map

--- selection_sort/src/main.rs
+++ selection_sort/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let unsort_massive = [43, 64, 85, 96, 45, 87, 23];
+    let sort_massive = unsort_massive
+        .map(|x| x + 1);
+    println!("{:?}", sort_massive);
+    // let mut sort_massive[i32; unsort_massive.len()] = [];
+    // let mut smallest = unsort_massive[0];
+    // let mut smallest_index = 0;
+    // for i in 1..unsort_massive.len() {
+    //     if unsort_massive[i] < smallest {
+    //         smallest = unsort_massive[i];
+    //         smallest_index = i;
+    //     }
+    //     println!("{}", );
+    // }
 }
