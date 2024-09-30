ae507934e16f888ad4cc88088d4a9a7764a8e835
Gregor1986 <egor953@yandex.ru>
2024-09-30 10:14:00 +0300

    Added func position_in_vec

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,21 +1,8 @@
 fn main() {
     let vec_a = create_vec(10000);
-    // println!("{:?}", vec_a);
     let item = 5;
-    let mut low = 0;
-    let mut high = vec_a.len() - 1;
-    while low <= high {
-        let mid = (low + high) / 2;
-        let guess = vec_a[mid];
-        if guess == item {
-            println!("Позиция элемента в векторе {}", mid);
-            break;
-        } else if guess > item {
-            high = mid - 1
-        } else {
-            low = mid + 1
-        }
-    }
+    position_in_vec(vec_a, item);
+    
 }
 fn create_vec(len_vec: i32) -> Vec<i32> {
     let mut a = vec![];
@@ -25,4 +12,20 @@ fn create_vec(len_vec: i32) -> Vec<i32> {
         i += 1;
     }
     a
+}
+fn position_in_vec(vec: Vec<i32>, item: i32) {
+    let mut low = 0;
+    let mut high = vec.len() - 1;
+    while low <= high {
+        let mid = (low + high) / 2;
+        let guess = vec[mid];
+        if guess == item {
+            println!("Позиция элемента в векторе {}", mid);
+            break;
+        } else if guess > item {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
 }
