2a67ba96df10fa13b50d89b2f4876d78e8926cf5
Gregor1986 <egor953@yandex.ru>
2024-10-01 15:10:48 +0300

    Added recursion func factorial

--- recursion/src/main.rs
+++ recursion/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let a = 5;
     countdown(a);
+    let b = 9;
+    println!("factorial {}", fact(b));
 }
 fn countdown(a: i32) {
     println!("{}", a);
@@ -10,4 +12,11 @@ fn countdown(a: i32) {
         countdown(a - 1);
     }
     
+}
+fn fact(b: i32) -> i32 {
+    if b == 1 {
+        b
+    } else {
+        b * fact(b - 1)
+    }
 }
