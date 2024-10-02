5cd8a415645904a76af06d4eec58972fa75f40ba
Gregor1986 <egor953@yandex.ru>
2024-10-02 09:32:26 +0300

    Added func sum_array

--- recursion/src/main.rs
+++ recursion/src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     countdown(a);
     let b = 9;
     println!("factorial {}", fact(b));
+    let array = [4, 6, 7, 8, 9];
+    println!("Сумма чисел в архиве: {}", sum_array(array));
 }
 fn countdown(a: i32) {
     println!("{}", a);
@@ -18,4 +20,11 @@ fn fact(b: i32) -> i32 {
     } else {
         b * fact(b - 1)
     }
+}
+fn sum_array(array: [i32; 5]) -> i32 {
+    let mut total = 0;
+    for i in array {
+        total += i;
+    }
+    total
 }
