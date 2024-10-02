ce19e9e2225b44847e5ce0fbec8b0f143e6384ec
Gregor1986 <egor953@yandex.ru>
2024-10-02 14:42:43 +0300

    Added func count elements

--- recursion/src/main.rs
+++ recursion/src/main.rs
@@ -3,8 +3,10 @@ fn main() {
     countdown(a);
     let b = 9;
     println!("factorial {}", fact(b));
-    let array = [4, 6, 7, 8, 9];
+    let array = [4, 6, 7, 8, 9, 56];
     println!("Сумма чисел в архиве: {}", sum_array(array));
+    println!("{}", sum_recursion(array));
+    println!("{}", count_element(array));
 }
 fn countdown(a: i32) {
     println!("{}", a);
@@ -21,10 +23,29 @@ fn fact(b: i32) -> i32 {
         b * fact(b - 1)
     }
 }
-fn sum_array(array: [i32; 5]) -> i32 {
+fn sum_array(array: [i32; 6]) -> i32 {
     let mut total = 0;
     for i in array {
         total += i;
     }
     total
+}
+fn sum_recursion(array: [i32; 6]) -> i32 {
+    let mut key = 0;
+    for i in 0..array.len() {
+        key = array[i];
+    }
+    key
+}
+fn count_element(array: [i32; 6]) -> i32 {
+    let mut count = 0;
+    // let mut key = 0;
+    for i in 0..array.len() {
+        if count > i {
+            count as i32;
+        } else {
+            count += 1;
+        }
+    }
+    count as i32
 }
