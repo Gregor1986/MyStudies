228c31e6821f1fa155705987245b6f27a8dfd9fc
Gregor1986 <egor953@yandex.ru>
2024-10-02 22:40:15 +0300

    Added test crates rand

--- fast_sort/src/main.rs
+++ fast_sort/src/main.rs
@@ -1,3 +1,11 @@
+use rand::{Rng, thread_rng};
 fn main() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    let x: u32 = rng.gen();
+    println!("{}", x);
+    let n: u32 = rng.gen_range(0..100);
+    println!("{}", n);
+    let mut arr = [0u8; 20];
+    thread_rng().fill(&mut arr[..]);
+    println!("{:?}", arr);
 }
