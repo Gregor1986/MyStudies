5ab71034c2e87ba55d6c19a778a64060108454dc
Gregor1986 <egor953@yandex.ru>
2024-10-03 14:05:31 +0300

    Clear project

--- fast_sort/src/main.rs
+++ fast_sort/src/main.rs
@@ -1,14 +1,14 @@
 use rand::{Rng, thread_rng};
 fn main() {
-    // let mut rng = thread_rng();
+    let mut rng = thread_rng();
     // let x: u32 = rng.gen();
     // println!("{}", x);
     // let n: u32 = rng.gen_range(0..100);
     // println!("{}", n);
-    // let mut arr = [0u8; 20];
-    // thread_rng().fill(&mut arr[..]);
-    // println!("{:?}", arr);
-    println!("{:?}", generate_vector(10));
+    let mut arr = [0u8; 20];
+    thread_rng().fill(&mut arr[..]);
+    println!("array: {:?}", arr);
+    println!("vector: {:?}", generate_vector(10));
 
 }
 fn generate_vector(lenght_vec: usize) -> Vec<i32> {
