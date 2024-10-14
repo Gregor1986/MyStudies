be02d4882400b7c572c51f0116dd84a0baf89721
Gregor1986 <egor953@yandex.ru>
2024-10-14 19:59:41 +0300

    Added test func: gen vec, averge sum array

--- main_test/src/main.rs
+++ main_test/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let a = [5, 6, 7, 3, 2, 1, 9];
+    println!("{}", average(&a));
+    let n: usize = 15;
+    let xs = random(n);
+    println!("{:?}", xs);
+}
+// Среднее значение сумма чисел в массиве
+fn average (array: &[i32]) -> f64 {
+    array.iter().fold(0, |x, y| x + y) as f64 / array.len() as f64
+}
+// Псевдо генератор вектора
+fn random(n: usize) -> Vec<u32> {
+    let mut r = 9;
+    std::iter::repeat_with(move || {
+        r ^= r << 3;
+        r ^= r >> 17;
+        r ^= r << 5;
+        r
+    }).take(n).collect()
 }
